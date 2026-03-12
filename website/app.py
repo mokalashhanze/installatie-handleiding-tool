93b5ac5b22e0c9aa6cf8e656a25c0a19cd8f16db
alexander <alexander.fluttert@gmail.com>
2026-03-13 00:14:20 +0100

    tekst van bcftools geupdate

--- website/app.py
+++ website/app.py
@@ -1,11 +1,24 @@
-from flask import Flask
-
+from flask import Flask, render_template
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home():
+    return render_template('index.html')
+@app.route('/bmf')
+def bmf():
+    return render_template('bmf.html')
+@app.route('/bwa-mem2')
+def bwamem2():
+    return render_template('bwa-mem2.html')
+@app.route('/samtools')
+def samtools():
+    return render_template('samtools.html')
+@app.route('/bcftools')
+def bcftools():
+    return render_template('bcftools.html')
+@app.route('/contacten')
+def contacten():
+    return render_template('contactgegevens.html')
 
 
 if __name__ == '__main__':
