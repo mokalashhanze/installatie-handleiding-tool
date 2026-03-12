87dac3491def95ae1b2d545fa6ed3d6812ca52cb
UpiPie <oa.dogger@st.hanze.nl>
2026-03-13 00:25:20 +0100

    BMF post en get files geadd

--- website/app.py
+++ website/app.py
@@ -1,12 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
-@app.route('/bmf')
+@app.route('/bmf', methods=['GET', 'POST'])
 def bmf():
-    return render_template('bmf.html')
+    if request.method == 'GET':
+        return render_template("bmf-get.html")
+    elif request.method == 'POST':
+        kwargs = {
+            "fastq_bestand" : request.form['fastq_bestand'],
+            "nucleotiden_achter_elkaar" : request.form['nucleotiden_achter_elkaar'],
+            "afstand_matches" : request.form['afstand_matches'],
+            "maximaal_keer_gerapporteerd" : request.form['maximaal_keer_gerapporteerd'],
+        }
+        return render_template("bmf-post.html", **kwargs)
+    return None
 @app.route('/bwa-mem2')
 def bwamem2():
     return render_template('bwa-mem2.html')
