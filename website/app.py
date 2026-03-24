9e42449e26c31b5f3af013c2904bf94271ca0a77
UpiPie <oa.dogger@st.hanze.nl>
2026-03-24 12:02:50 +0100

    added threading merged post and get bmf added tools

--- website/app.py
+++ website/app.py
@@ -1,21 +1,40 @@
-from flask import Flask, render_template, request
+from flask import request, render_template, Flask
+from threading import Thread
+import tools
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
+
+def pijplijn(**kwargs):
+    tools.main2(kwargs)
 @app.route('/bmf', methods=['GET', 'POST'])
 def bmf():
-    if request.method == 'GET':
-        return render_template("bmf-get.html")
-    elif request.method == 'POST':
+    method = request.method
+    if method == 'GET':
+        return render_template("bmf-get.html", method=method)
+    elif method == 'POST':
         kwargs = {
             "fastq_bestand" : request.form['fastq_bestand'],
-            "nucleotiden_achter_elkaar" : request.form['nucleotiden_achter_elkaar'],
-            "afstand_matches" : request.form['afstand_matches'],
-            "maximaal_keer_gerapporteerd" : request.form['maximaal_keer_gerapporteerd'],
+            "k" : request.form['nucleotiden_achter_elkaar'],
+            "w" : request.form['afstand_matches'],
+            "c" : request.form['maximaal_keer_gerapporteerd'],
         }
-        return render_template("bmf-post.html", **kwargs)
+
+        # Hiermee checken we of de file correct is, als niet word het niet door gestuurd.
+        file_name = kwargs["fastq_bestand"]
+        file_type = file_name.rsplit(".", 1)[-1]
+
+        if file_type == "fastq" or file_type == "fq":
+            #Hiermee beginnen we een andere thread zodat we veranderen van pagina voordat het af is om te laten zien dat
+            thread = Thread(target=pijplijn, kwargs=kwargs, daemon=True)
+            thread.start()
+
+            return render_template("bmf-get.html", **kwargs, method=method)
+        else:
+            method = "GET"
+            return render_template("bmf-get.html", **kwargs, method=method)
     return None
 @app.route('/bwa-mem2')
 def bwamem2():
