6fc9c5c98641eeae78bafd080bf6bcc2281111c7
UpiPie <oa.dogger@st.hanze.nl>
2026-03-25 14:37:51 +0100

    End pagina gemaakt die werkt op progess en prevous run laten zien op input

--- website/app.py
+++ website/app.py
@@ -1,53 +1,90 @@
-from flask import request, render_template, Flask
+from flask import request, render_template, Flask, send_file
 from threading import Thread
 import tools
+import os
+
 app = Flask(__name__)
+
+# Globale status
+job_status = "idle"
+kwargs = {}
+vorige_kwargs = {}
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
 def pijplijn(**kwargs):
-    tools.main2(kwargs)
+    global job_status
+    tools.main(kwargs)
+    job_status = "done"
+
 @app.route('/bmf', methods=['GET', 'POST'])
 def bmf():
-    method = request.method
-    if method == 'GET':
-        return render_template("bmf-get.html", method=method)
-    elif method == 'POST':
+    global job_status, kwargs, vorige_kwargs
+
+    # GET
+    if request.method == 'GET':
+        #Pijplijn draait nog
+        if job_status == "running":
+            return render_template("bmf-get.html",method="STATUS",status="running",**kwargs)
+
+        #Pijplijn is klaar
+        if job_status == "done":
+            # Vorige_kwargs opslaan om te laten zien onder aan de input pagina voor al is de gebruiker te snel weggegaan
+            vorige_kwargs = kwargs
+            vorige_kwargs["output"] = "out.vcf"
+            # Job_status resetten voor volgende keer user gebruik en kwargs
+            job_status = "idle"
+            kwargs = {}
+            return render_template("bmf-get.html",method="STATUS",status="done", **vorige_kwargs)
+
+        return render_template("bmf-get.html", method="GET", vorige=vorige_kwargs)
+
+    # ---------------- POST ----------------
+    elif request.method == 'POST':
         kwargs = {
-            "fastq_bestand" : request.form['fastq_bestand'],
-            "k" : request.form['nucleotiden_achter_elkaar'],
-            "w" : request.form['afstand_matches'],
-            "c" : request.form['maximaal_keer_gerapporteerd'],
+            "fastq_bestand": request.form['fastq_bestand'],
+            "k": request.form['nucleotiden_achter_elkaar'],
+            "w": request.form['afstand_matches'],
+            "c": request.form['maximaal_keer_gerapporteerd'],
         }
 
-        # Hiermee checken we of de file correct is, als niet word het niet door gestuurd.
         file_name = kwargs["fastq_bestand"]
         file_type = file_name.rsplit(".", 1)[-1]
 
-        if file_type == "fastq" or file_type == "fq":
-            #Hiermee beginnen we een andere thread zodat we veranderen van pagina voordat het af is om te laten zien dat
-            thread = Thread(target=pijplijn, kwargs=kwargs, daemon=True)
-            thread.start()
+        if file_type not in ("fastq", "fq"):
+            return render_template("bmf-get.html", method="GET", **kwargs)
 
-            return render_template("bmf-get.html", **kwargs, method=method)
-        else:
-            method = "GET"
-            return render_template("bmf-get.html", **kwargs, method=method)
+        job_status = "running"
+
+        thread = Thread(target=pijplijn, kwargs=kwargs, daemon=True)
+        thread.start()
+
+        return render_template("bmf-get.html", method="STATUS", **kwargs)
     return None
+
 @app.route('/bwa-mem2')
 def bwamem2():
     return render_template('bwa-mem2.html')
+
 @app.route('/samtools')
 def samtools():
     return render_template('samtools.html')
+
 @app.route('/bcftools')
 def bcftools():
     return render_template('bcftools.html')
+
 @app.route('/contacten')
 def contacten():
     return render_template('contactgegevens.html')
+
+@app.route('/bmf/download')
+def download():
+    file_path = os.path.join(app.root_path, "out.vcf")
+    return send_file(file_path,as_attachment=True,download_name="out.vcf")
+
 
 
 if __name__ == '__main__':
--- website/tools.py
+++ website/tools.py
@@ -1,24 +1,37 @@
 import subprocess
+import time
 
 class Tools():
     pass
 
 
-def main():
-    subprocess.run(f"bwa-mem2 mem GCF_000001405……fna sequence_test.fastq > output.bam")
-    subprocess.run("samtools view -b -o samtools_test.bam output.sam")
-    subprocess.run("samtools sort samtoolstest.bam > sorted_samtools.bam")
-    subprocess.run("bcftools mpileup sorted_samtools.bam -f GCF_000001405.40…..fna > bcftools_mpileup.bam")
-    subprocess.run("bcftools call -m -O v -o out.vcf bcftools_mpileup.bam")
-
-def main2(kwargs):
+def main(kwargs):
     print(kwargs)
     print(kwargs["k"])
     print(kwargs["fastq_bestand"])
-
-    #subprocess.run(f"bwa-mem2 mem -k {kwargs["k"]} -w {kwargs["w"]} -c {kwargs["c"]}  INDEX-BESTAND {"fastq-bestand"} > output.bam")
-    #subprocess.run("samtools view -b -o samtools_test.bam output.sam")
-    #subprocess.run("samtools sort samtoolstest.bam > sorted_samtools.bam")
-    #subprocess.run("bcftools mpileup sorted_samtools.bam -f GCF_000001405.40…..fna > bcftools_mpileup.bam")
-    #subprocess.run("bcftools call -m -O v -o out.vcf bcftools_mpileup.bam")
+    time.sleep(10)
+    # # Run bwa-mem2
+    # subprocess.run(
+    #     f"/homes/oadogger/Desktop/toolbox/bwa-mem2-2.3_x64-linux/bwa-mem2 mem -k {kwargs['k']} -w {kwargs['w']} -c {kwargs['c']} GCF_000001405.40_GRCh38.p14_genomic.fna {kwargs['fastq_bestand']} > output.sam",
+    #     shell=True
+    # )
+    # print("BWA-mem2")
+    # #Output.bam -> output.sam
+    # subprocess.run(
+    #     "/homes/oadogger/Desktop/toolbox/samtools-1.23/bin/samtools view -b -o output.bam output.sam",
+    #      shell=True
+    # )
+    # # Sort de bam file
+    # subprocess.run(
+    #     "/homes/oadogger/Desktop/toolbox/samtools-1.23/bin/samtools sort output.bam > sorted_output.bam",
+    #     shell=True
+    # )
+    #
+    # subprocess.run(
+    #     "/homes/oadogger/Desktop/toolbox/bcftools-1.23/bcftools mpileup sorted_output.bam -f GCF_000001405.40_GRCh38.p14_genomic.fna > bcftools_mpileup.bam",
+    #     shell=True
+    # )
+    # subprocess.run(
+    #     "/homes/oadogger/Desktop/toolbox/bcftools-1.23/bcftools call -m -O v -o out.vcf bcftools_mpileup.bam",
+    #     shell=True)
     print(f"done!")
